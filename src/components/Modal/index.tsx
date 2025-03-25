019518304d8fab563d5795794ba4bab22131ca63
Luiz Guilherme <luizgmelo64@gmail.com>
2025-03-25 20:39:23 -0300

    refac: remove funcions from modal to app parent component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,71 +1,30 @@
-import { useState } from "react";
-import TaskType from "../../types/TaskType";
 import InputTextField from "../ui/InputTextField";
 import style from "./modal.module.scss";
 
 interface IModal {
-  title: string;
-  isOpen: boolean;
-  setOpen: (isOpen: boolean) => void;
-  createTask: (task: TaskType) => void;
-  onToggle: (id: string) => void;
-  onDelete: (id: string) => void;
+  input: string;
+  onChange: (input: string) => void;
+  onSubmit: (input: string) => void;
+  onClose: () => void;
 }
 
-export default function Modal({
-  title,
-  isOpen,
-  setOpen,
-  createTask,
-  onToggle,
-  onDelete,
-}: IModal) {
-  const [idCounter, setId] = useState<number>(0);
-  const [inputValue, setInputValue] = useState<string>("");
-
-  const closeModal = () => {
-    setInputValue("");
-    setOpen(!isOpen);
-  };
-
-  const applyNewTask = () => {
-    if (inputValue === "" || inputValue === undefined) {
-      return;
-    }
-
-    setId((idCounter) => ++idCounter);
-
-    const newTask = {
-      id: idCounter.toString(),
-      title: inputValue,
-      isCompleted: false,
-      onToggle: onToggle,
-      onDelete: onDelete,
-    };
-
-    createTask(newTask);
-
-    closeModal();
-  };
-
-  return isOpen ? (
+export default function Modal({ input, onSubmit, onChange, onClose }: IModal) {
+  return (
     <div className={style.background}>
       <div className={style.modal}>
-        <h2>{title}</h2>
+        <h2>NEW NOTE</h2>
         <InputTextField
           placeholder="Input your note..."
-          value={inputValue}
-          onChange={setInputValue}
+          value={input}
+          onChange={onChange}
         />
         <div className={style.actions}>
-          <button onClick={() => closeModal()} className={style.btnCancel}>
+          <button onClick={() => onClose()} className={style.btnCancel}>
             Cancel
           </button>
-          <button onClick={() => applyNewTask()}>Apply</button>
+          <button onClick={() => onSubmit(input)}>Apply</button>
         </div>
       </div>
     </div>
-  ) : (
-    <></>
   );
 }

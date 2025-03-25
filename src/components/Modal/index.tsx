328774324633a4e94f6d3f203948522057b57868
Luiz Guilherme <luizgmelo64@gmail.com>
2025-03-25 12:24:26 -0300

    feat: create and update tasks

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,22 +1,67 @@
+import { useState } from "react";
+import TaskType from "../../types/TaskType";
 import InputTextField from "../ui/InputTextField";
 import style from "./modal.module.scss";
 
 interface IModal {
+  title: string;
   isOpen: boolean;
   setOpen: (isOpen: boolean) => void;
+  createTask: (task: TaskType) => void;
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
 }
 
-export default function Modal({ isOpen, setOpen }: IModal) {
+export default function Modal({
+  title,
+  isOpen,
+  setOpen,
+  createTask,
+  onToggle,
+  onDelete,
+}: IModal) {
+  const [idCounter, setId] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const closeModal = () => {
+    setInputValue("");
+    setOpen(!isOpen);
+  };
+
+  const applyNewTask = () => {
+    if (inputValue === "" || inputValue === undefined) {
+      return;
+    }
+
+    setId((idCounter) => ++idCounter);
+
+    const newTask = {
+      id: idCounter.toString(),
+      title: inputValue,
+      isCompleted: false,
+      onToggle: onToggle,
+      onDelete: onDelete,
+    };
+
+    createTask(newTask);
+
+    closeModal();
+  };
+
   return isOpen ? (
     <div className={style.background}>
       <div className={style.modal}>
-        <h2>NEW NOTE</h2>
-        <InputTextField placeholder="Input your note..." />
+        <h2>{title}</h2>
+        <InputTextField
+          placeholder="Input your note..."
+          value={inputValue}
+          onChange={setInputValue}
+        />
         <div className={style.actions}>
-          <button onClick={() => setOpen(!isOpen)} className={style.btnCancel}>
+          <button onClick={() => closeModal()} className={style.btnCancel}>
             Cancel
           </button>
-          <button>Apply</button>
+          <button onClick={() => applyNewTask()}>Apply</button>
         </div>
       </div>
     </div>
--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,16 +1,31 @@
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import styles from "./task.module.scss";
+import ITask from "../../types/TaskType";
 
-export default function Task() {
+export default function Task({
+  id,
+  title,
+  isCompleted,
+  onToggle,
+  onDelete,
+}: ITask) {
   return (
     <div className={styles.container}>
       <div className={styles.info}>
-        <input id="checkbox-1" type="checkbox" />
-        <label htmlFor="checkbox-1"> Nome da Tarefa</label>
+        <input
+          id={`checkbox-${id}`}
+          type="checkbox"
+          checked={isCompleted}
+          onChange={() => onToggle(id)}
+        />
+        <label htmlFor={`checkbox-${id}`}>{title}</label>
       </div>
       <div className={styles.actions}>
         <EditOutlined className={styles.editIcon} />
-        <DeleteOutlined className={styles.deleteIcon} />
+        <DeleteOutlined
+          className={styles.deleteIcon}
+          onClick={() => onDelete(id)}
+        />
       </div>
     </div>
   );

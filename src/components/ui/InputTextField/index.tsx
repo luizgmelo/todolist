67c5dddb6fa28af9de77e5b955e6ba17cfdd0ee1
Luiz Guilherme <luizgmelo64@gmail.com>
2025-03-25 12:23:18 -0300

    feat: update props input component

--- src/components/ui/InputTextField/index.tsx
+++ src/components/ui/InputTextField/index.tsx
@@ -3,16 +3,25 @@ import styles from "./input-text-fiels.module.scss";
 
 interface InputTextFieldProps {
   placeholder: string;
+  value: string;
   isSearch?: boolean;
+  onChange: (newValue: string) => void;
 }
 
 export default function InputTextField({
   placeholder,
   isSearch,
+  value,
+  onChange,
 }: InputTextFieldProps) {
   return (
     <div className={styles.container}>
-      <input type="text" placeholder={placeholder} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
       {isSearch ? <SearchOutlined className={styles.icon} /> : null}
     </div>
   );
